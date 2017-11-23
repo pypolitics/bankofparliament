3fd77c679700360fea136baab1f578c895fd194e
elliott <elliott@elliotts-MacBook-Pro.local>
2017-11-23 00:16:56 +0000

    Updating resize script.

--- bin/resize_images.sh
+++ bin/resize_images.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for i in ../lib/images/* ; do
-  if [ -f "$i" ]; then
-    echo "$i"
-    oiiotool "$i" --resize 128x128 -o "$i.jpg"
-  fi
-done
+# for i in ../lib/data/images/4* ; do
+#   if [ -f "$i" ]; then
+#     echo "$i"
+#     oiiotool "$i" --resize 128x128 -o "$i"
+#   fi
+# done
+
+oiiotool ../lib/data/images/male.png --resize 128x128 -o ../lib/data/images/male.png
+oiiotool ../lib/data/images/female.png --resize 128x128 -o ../lib/data/images/female.png
