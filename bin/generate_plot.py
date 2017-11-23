41ccfd664b9770cbe715ea575de9b1de3f8eb8a1
elliott <elliott@elliotts-MacBook-Pro.local>
2017-11-23 20:21:11 +0000

    Fixing generate_plot script and updating plots.

--- bin/generate_plot.py
+++ bin/generate_plot.py
@@ -4,15 +4,20 @@
 import json, os, sys
 from optparse import OptionParser
 sys.path.append('../lib/python')
+lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'lib')
 
-from utils import write_scatter_plot, read_json_file
+from utils import write_scatter_plot, read_json_file, read_expenses, match_expenses
+
+expenses_data_paths = {
+            # '2017-2018' : '../lib/data/export_17_18.csv',
+            '2016-2017' : '../lib/data/export_16_17.csv'
+            }
+
+expenses_data = read_expenses(expenses_data_paths)
 
 if __name__ == "__main__":
     """Commandline run"""
     parser = OptionParser()
-
-    # parser.add_option("--summary", help="Summary print", action="store_true", default=True)
-    # parser.add_option("--detailed", help="Detailed print", action="store_true", default=False)
     parser.add_option("--sortby", help="Sort By",
                       action="store", default='income')
 
@@ -37,4 +42,10 @@ if __name__ == "__main__":
 
     for mp in mps:
         print '\nProcessing Plot : %s' % mp['name']
-        write_scatter_plot(mp)
+        plot_file = os.path.join(lib_path, 'data', 'plots', '%s.json' % mp['member_id'])
+
+        # add expenses
+        for year in expenses_data.keys():
+            mp['expenses'] = match_expenses(mp, year, expenses_data)
+
+        write_scatter_plot(mp, plot_file)
