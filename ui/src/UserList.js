9fbbf491e20bd72da667ac557c6782f7b6d43ef4
elliott <elliott@elliotts-MacBook-Pro.local>
2019-04-19 20:12:30 +0100

    React / Apollo / GraphQL UI basic functionality working

---
+++ ui/src/UserList.js
@@ -0,0 +1,113 @@
+import React from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import "./UserList.css";
+import { withStyles } from "@material-ui/core/styles";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+  Tooltip,
+  Paper,
+  TableSortLabel
+} from "@material-ui/core";
+
+const styles = theme => ({
+  root: {
+    maxWidth: 700,
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto",
+    margin: "auto"
+  },
+  table: {
+    minWidth: 700
+  }
+});
+
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      order: "asc",
+      orderBy: "name",
+      page: 0,
+      rowsPerPage: 10
+    };
+  }
+
+  handleSortRequest = property => {
+    const orderBy = property;
+    let order = "desc";
+
+    if (this.state.orderBy === property && this.state.order === "desc") {
+      order = "asc";
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  render() {
+    const { order, orderBy } = this.state;
+    return (
+      <Query
+        query={gql`
+          {
+            Person(first: 10) {
+              name
+            }
+          }
+        `}
+      >
+        {({ loading, error, data }) => {
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error</p>;
+
+          return (
+            <Paper className={this.props.classes.root}>
+              <Table className={this.props.classes.table}>
+                <TableHead>
+                  <TableRow>
+                    <TableCell
+                      key="name"
+                      sortDirection={orderBy === "name" ? order : false}
+                    >
+                      <Tooltip
+                        title="Sort"
+                        placement="bottom-start"
+                        enterDelay={300}
+                      >
+                        <TableSortLabel
+                          active={orderBy === "name"}
+                          direction={order}
+                          onClick={() => this.handleSortRequest("name")}
+                        >
+                          Name
+                        </TableSortLabel>
+                      </Tooltip>
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {data.Person.map(n => {
+                    return (
+                      <TableRow key={n.id}>
+                        <TableCell component="th" scope="row">
+                          {n.name}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </Paper>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default withStyles(styles)(UserList);
