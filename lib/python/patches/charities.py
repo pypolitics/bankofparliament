c43078b2d6f40ddede290c1d911c59101a12b15e
elliott <elliott@elliotts-MacBook-Pro.local>
2019-04-19 16:41:16 +0100

    Clean slate

--- lib/python/patches/charities.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-charities = {}
-charities['Catholic Bishops’ Conference of England and Wales'] = 'Catholic Bishops Conference of England and Wales'
-charities['CARE (Christian Action Research and Education)'] = 'Christian Action Research and Education'
-charities['The British Council'] = 'The British Council'
-charities['The Public Interest Foundation (UK charity)'] = 'The Public Interest Foundation'
